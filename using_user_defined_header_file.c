52b409bab95aad2d2e1a27dfa044c277b61b65b1
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-02-24 11:25:46 +0530

    Create using_user_defined_header_file.c

---
+++ using_user_defined_header_file.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include "myhead.h" 
+int main() 
+{ 
+    add(4, 6); 
+  
+    /*This calls add function written in myhead.h   
+      and therefore no compilation error.*/
+    multiply(5, 5); 
+  
+    // Same for the multiply function in myhead.h 
+    printf("BYE!See you Soon"); 
+    return 0; 
+} 
